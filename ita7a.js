3849a6d07ed110bb1ce09c45b2ef5702311fa79a
ita7a <57022144+ita7a@users.noreply.github.com>
2021-01-10 06:05:12 +0300

    Create ita7a.js

---
+++ ita7a.js
@@ -0,0 +1,18 @@
+$(`<img tabindex="0" class="al120 fl " style="margin-left: -4px; padding:5px; width:40px;margin-top: -4px;" src="/sico/1558730372080.png" data-original-title="" title="">`).insertAfter('img.fl.nosel.emobox');
+$(".al120").click(function(){
+alert("عذرا لا تملك الصلاحية للدخول  ");
+});
+
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><input type="ita7a.com" name="FirstName" value=" ITA7A | FM روتانا  ">
+<audio class="wp-audio-shortcode" id="audio-236-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="http://curiosity.shoutca.st:6035/stream"><a href="https://www.bbc.com/ws/av-embeds/media/bbc_arabic_radio/liveradio/ar</a></audio>
+<center></center></center>
+</div>`);
+$('#d0').append(`<label title="اذاعه التحليه" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
+data-toggle="tab" data-target="#mic"  class="ae fa label label-primary fa-microphone ">FM</label>`);
+$('.u-msg.dots').removeClass('dots');
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="ita7a.com" name="FirstName" value=" ITA7A | الاخبار "><iframe class="fr" style="width:100%;margin-top: 0px;" src="https://www.bbc.com/ws/av-embeds/media/bbc_arabic_radio/liveradio/ar" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><center>`).appendTo
+("#mic"),clearInterval(b)):console.log(c)},1)});
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="ita7a.com" name="FirstName" value="ITA7A | FM ألف "><audio src="http://www.alifaliffm.com:8800/AlifAlif.mp3" controls loop></audio></div><center>`).appendTo
+("#mic"),clearInterval(b)):console.log(c)},1)});
+
+$(`<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">\x3C\x61\x20\x68\x72\x65\x66=\u0022aHR0cHM6Ly9pdGE3YS5jb20v\u0022 target="_blank" id=""><\u0069\u006D\u0067 \x73\x72\x63\x3D"\u002F\u0073\u0069\u0063\u006F\u002F\u0031\u0035\u0039\u0032\u0039\u0032\u0034\u0033\u0031\u0039\u0032\u0033\u0038\u002E\u0067\u0069\u0066" style="width: 70%;transform:matrix(1, 0, 0, 1, 8, 0);"></a></marquee></font></b></td>`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
